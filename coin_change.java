645e6edd20b6f7ca2d725192d806445e60a2aa96
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 14:50:20 -0700

    codex-exp: iteration 1

--- coin_change.java
+++ coin_change.java
@@ -1,26 +1,18 @@
-import java.util.Arrays;
-
-public class CoinChange{
+class CoinChange {
 
     static int count(int[] coins, int sum) {
-        int n = coins.length;
+        int[] ways = new int[sum + 1];
+        ways[0] = 1;
 
-        int[][] dp = new int[n + 1][sum + 1];
-
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
-
-                dp[i][j] += dp[i - 1][j];
-
-                if (j - coins[i - 1] >= 0) {
-
-                    dp[i][j] += dp[i][j - coins[i - 1]];
-                }
+        for (int coin : coins) {
+            for (int currentSum = coin; currentSum <= sum; currentSum++) {
+                ways[currentSum] += ways[currentSum - coin];
             }
         }
-        return dp[n][sum];
-}
+
+        return ways[sum];
+    }
+
     public static void main(String[] args) {
         int[] coins = {1, 2, 3};
         int sum = 5;

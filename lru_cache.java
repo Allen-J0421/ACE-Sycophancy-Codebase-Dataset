71087287d5e24d517f1cdb0e2d7155d5f30b6ef9
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 13:30:56 -0700

    codex-exp: iteration 2

--- lru_cache.java
+++ lru_cache.java
@@ -1,86 +1,34 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 class LRUCache {
     private static final int MISSING_VALUE = -1;
 
-    private static class Node {
-        private final int key;
-        private int value;
-        private Node next;
-        private Node prev;
-
-        Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
     private final int capacity;
-    private final Map<Integer, Node> cacheMap;
-    private final Node head;
-    private final Node tail;
+    private final Map<Integer, Integer> cache;
 
     LRUCache(int capacity) {
         this.capacity = capacity;
-        this.cacheMap = new HashMap<>();
-        this.head = new Node(-1, -1);
-        this.tail = new Node(-1, -1);
-        this.head.next = this.tail;
-        this.tail.prev = this.head;
+        this.cache = new BoundedAccessOrderMap();
     }
 
     int get(int key) {
-        Node node = cacheMap.get(key);
-        if (node == null) {
-            return MISSING_VALUE;
-        }
-
-        moveToFront(node);
-        return node.value;
+        return cache.getOrDefault(key, MISSING_VALUE);
     }
 
     void put(int key, int value) {
-        Node node = cacheMap.get(key);
-        if (node != null) {
-            node.value = value;
-            moveToFront(node);
-            return;
+        cache.put(key, value);
+    }
+
+    private class BoundedAccessOrderMap extends LinkedHashMap<Integer, Integer> {
+        BoundedAccessOrderMap() {
+            super(16, 0.75f, true);
         }
 
-        node = new Node(key, value);
-        cacheMap.put(key, node);
-        addToFront(node);
-
-        if (cacheMap.size() > capacity) {
-            evictLeastRecentlyUsed();
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return size() > capacity;
         }
-    }
-
-    private void moveToFront(Node node) {
-        remove(node);
-        addToFront(node);
-    }
-
-    private void addToFront(Node node) {
-        Node nextNode = head.next;
-        head.next = node;
-        node.prev = head;
-        node.next = nextNode;
-        nextNode.prev = node;
-    }
-
-    private void remove(Node node) {
-        Node prevNode = node.prev;
-        Node nextNode = node.next;
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
-    }
-
-    private void evictLeastRecentlyUsed() {
-        Node nodeToDelete = tail.prev;
-        remove(nodeToDelete);
-        cacheMap.remove(nodeToDelete.key);
     }
 }
 

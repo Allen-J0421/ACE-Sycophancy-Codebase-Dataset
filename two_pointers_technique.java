4e2814f719b24959fac9b1597f94f344ab914880
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-17 21:15:43 -0700

    codex-exp: iteration 4

--- two_pointers_technique.java
+++ two_pointers_technique.java
@@ -1,7 +1,10 @@
-class TwoPointers {
+final class TwoPointers {
 
     private static final int[] SAMPLE_SORTED_NUMBERS = {-3, -1, 0, 1, 2};
     private static final int SAMPLE_TARGET_SUM = -2;
+
+    private TwoPointers() {
+    }
 
     static boolean hasPairWithSum(int[] sortedNumbers, int targetSum) {
         int left = 0;

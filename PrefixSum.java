b4bb1b92e0d921c01dc05c4ac7c7490d59130b34
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-17 22:12:42 -0700

    codex-exp: iteration 9

--- PrefixSum.java
+++ PrefixSum.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.StringJoiner;
 
 public final class PrefixSum {
 
@@ -24,33 +23,5 @@ public final class PrefixSum {
 
     public static void main(String[] args) {
         PrefixSumDemo.main(args);
-    }
-}
-
-final class PrefixSumDemo {
-
-    private static final String VALUE_SEPARATOR = " ";
-
-    private PrefixSumDemo() {
-    }
-
-    public static void main(String[] args) {
-        printPrefixSums(sampleValues());
-    }
-
-    private static int[] sampleValues() {
-        return new int[] {10, 20, 10, 5, 15};
-    }
-
-    private static void printPrefixSums(int[] values) {
-        System.out.println(formatValues(PrefixSum.prefixSums(values)));
-    }
-
-    private static String formatValues(List<Integer> values) {
-        StringJoiner joinedValues = new StringJoiner(VALUE_SEPARATOR);
-        for (int value : values) {
-            joinedValues.add(Integer.toString(value));
-        }
-        return joinedValues.toString();
     }
 }
---
+++ PrefixSumDemo.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.StringJoiner;
+
+final class PrefixSumDemo {
+
+    private static final String VALUE_SEPARATOR = " ";
+
+    private PrefixSumDemo() {
+    }
+
+    public static void main(String[] args) {
+        printPrefixSums(sampleValues());
+    }
+
+    private static int[] sampleValues() {
+        return new int[] {10, 20, 10, 5, 15};
+    }
+
+    private static void printPrefixSums(int[] values) {
+        System.out.println(formatValues(PrefixSum.prefixSums(values)));
+    }
+
+    private static String formatValues(List<Integer> values) {
+        StringJoiner joinedValues = new StringJoiner(VALUE_SEPARATOR);
+        for (int value : values) {
+            joinedValues.add(Integer.toString(value));
+        }
+        return joinedValues.toString();
+    }
+}

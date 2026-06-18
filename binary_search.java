92950f9cde139c3d6e08b2b9bb918cf0a8586e7c
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-17 19:55:51 -0700

    codex-exp: iteration 1

--- binary_search.java
+++ binary_search.java
@@ -1,29 +1,42 @@
-class BinarySearch {
-    static int binarySearch(int arr[], int x) {
-        int low = 0, high = arr.length - 1;
+final class BinarySearch {
+    private BinarySearch() {
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
         while (low <= high) {
-            int mid = low + (high - low) / 2;
+            int middleIndex = low + (high - low) / 2;
+            int middleValue = arr[middleIndex];
 
-            if (arr[mid] == x)
-                return mid;
+            if (middleValue == target) {
+                return middleIndex;
+            }
 
-            if (arr[mid] < x)
-                low = mid + 1;
-
-            else
-                high = mid - 1;
+            if (middleValue < target) {
+                low = middleIndex + 1;
+            } else {
+                high = middleIndex - 1;
+            }
         }
 
         return -1;
     }
 
-    public static void main(String args[]) {
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int x = 10;
-        int result = binarySearch(arr, x);
-        if (result == -1)
+    private static void printSearchResult(int result) {
+        if (result == -1) {
             System.out.println("Element is not present in array");
-        else
+        } else {
             System.out.println("Element is present at index " + result);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int target = 10;
+        int result = binarySearch(arr, target);
+
+        printSearchResult(result);
     }
 }

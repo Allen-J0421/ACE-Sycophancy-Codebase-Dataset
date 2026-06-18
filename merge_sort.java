939532cb3ce26df88cf028a9522bcd01f37b2416
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 05:11:09 -0700

    codex-exp: iteration 1

--- merge_sort.java
+++ merge_sort.java
@@ -1,70 +1,75 @@
-import java.io.*;
-
 class MergeSort {
 
-    static void merge(int arr[], int l, int m, int r){
+    private MergeSort() {
+    }
 
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0;
-
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
+    static void sort(int[] values) {
+        if (values == null || values.length < 2) {
+            return;
         }
 
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        mergeSort(values, 0, values.length - 1);
+    }
+
+    private static void mergeSort(int[] values, int left, int right) {
+        if (left >= right) {
+            return;
         }
 
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        int middle = left + (right - left) / 2;
+
+        mergeSort(values, left, middle);
+        mergeSort(values, middle + 1, right);
+        merge(values, left, middle, right);
+    }
+
+    private static void merge(int[] values, int left, int middle, int right) {
+        int leftSize = middle - left + 1;
+        int rightSize = right - middle;
+
+        int[] leftValues = new int[leftSize];
+        int[] rightValues = new int[rightSize];
+
+        for (int i = 0; i < leftSize; i++) {
+            leftValues[i] = values[left + i];
+        }
+
+        for (int i = 0; i < rightSize; i++) {
+            rightValues[i] = values[middle + 1 + i];
+        }
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int mergedIndex = left;
+
+        while (leftIndex < leftSize && rightIndex < rightSize) {
+            if (leftValues[leftIndex] <= rightValues[rightIndex]) {
+                values[mergedIndex++] = leftValues[leftIndex++];
+            } else {
+                values[mergedIndex++] = rightValues[rightIndex++];
+            }
+        }
+
+        while (leftIndex < leftSize) {
+            values[mergedIndex++] = leftValues[leftIndex++];
+        }
+
+        while (rightIndex < rightSize) {
+            values[mergedIndex++] = rightValues[rightIndex++];
         }
     }
 
-    static void mergeSort(int arr[], int l, int r){
+    public static void main(String[] args) {
+        int[] values = {38, 27, 43, 10};
 
-        if (l < r) {
-
-            int m = l + (r - l) / 2;
-
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
-
-            merge(arr, l, m, r);
-        }
+        sort(values);
+        printValues(values);
     }
 
-    public static void main(String args[]){
-
-        int arr[] = {38, 27, 43, 10};
-
-        mergeSort(arr, 0, arr.length - 1);
-
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
+    private static void printValues(int[] values) {
+        for (int value : values) {
+            System.out.print(value + " ");
+        }
         System.out.println();
     }
 }

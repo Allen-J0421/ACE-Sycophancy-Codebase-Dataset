28f6a5d0defc0cc6d252a0060756c1a531036a35
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-17 22:15:29 -0700

    codex-exp: iteration 2

--- PrefixSum.java
+++ PrefixSum.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public final class PrefixSum {
@@ -7,26 +5,38 @@ public final class PrefixSum {
     private PrefixSum() {
     }
 
-    public static List<Integer> build(int[] values) {
+    public static int[] build(int[] values) {
         Objects.requireNonNull(values, "values must not be null");
 
-        List<Integer> prefixSums = new ArrayList<>(values.length);
+        int[] prefixSums = new int[values.length];
         int runningTotal = 0;
 
-        for (int value : values) {
-            runningTotal += value;
-            prefixSums.add(runningTotal);
+        for (int i = 0; i < values.length; i++) {
+            runningTotal += values[i];
+            prefixSums[i] = runningTotal;
         }
 
         return prefixSums;
     }
 
+    public static String format(int[] values) {
+        Objects.requireNonNull(values, "values must not be null");
+
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            if (i > 0) {
+                output.append(' ');
+            }
+            output.append(values[i]);
+        }
+
+        return output.toString();
+    }
+
     public static void main(String[] args) {
         int[] values = {10, 20, 10, 5, 15};
-        List<Integer> prefixSums = build(values);
-
-        for (int prefixSum : prefixSums) {
-            System.out.print(prefixSum + " ");
-        }
+        int[] prefixSums = build(values);
+        System.out.println(format(prefixSums));
     }
 }

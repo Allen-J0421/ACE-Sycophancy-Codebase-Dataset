bc3cb990f9389a469e3885796d28a5830023be56
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 05:20:40 -0700

    codex-exp: iteration 1

--- merge_sort.java
+++ merge_sort.java
@@ -1,70 +1,66 @@
-import java.io.*;
-
 class MergeSort {
 
-    static void merge(int arr[], int l, int m, int r){
+    private MergeSort() {
+        // Utility class.
+    }
 
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0;
-
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
+    static void sort(int[] values) {
+        if (values == null || values.length < 2) {
+            return;
         }
 
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        int[] scratch = new int[values.length];
+        mergeSort(values, scratch, 0, values.length - 1);
+    }
+
+    private static void mergeSort(int[] values, int[] scratch, int left, int right) {
+        if (left >= right) {
+            return;
         }
 
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        int middle = left + (right - left) / 2;
+        mergeSort(values, scratch, left, middle);
+        mergeSort(values, scratch, middle + 1, right);
+        merge(values, scratch, left, middle, right);
+    }
+
+    private static void merge(int[] values, int[] scratch, int left, int middle, int right) {
+        int leftIndex = left;
+        int rightIndex = middle + 1;
+        int scratchIndex = left;
+
+        while (leftIndex <= middle && rightIndex <= right) {
+            if (values[leftIndex] <= values[rightIndex]) {
+                scratch[scratchIndex++] = values[leftIndex++];
+            } else {
+                scratch[scratchIndex++] = values[rightIndex++];
+            }
+        }
+
+        while (leftIndex <= middle) {
+            scratch[scratchIndex++] = values[leftIndex++];
+        }
+
+        while (rightIndex <= right) {
+            scratch[scratchIndex++] = values[rightIndex++];
+        }
+
+        for (int index = left; index <= right; index++) {
+            values[index] = scratch[index];
         }
     }
 
-    static void mergeSort(int arr[], int l, int r){
-
-        if (l < r) {
-
-            int m = l + (r - l) / 2;
-
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
-
-            merge(arr, l, m, r);
+    private static void printArray(int[] values) {
+        for (int value : values) {
+            System.out.print(value + " ");
         }
-    }
-
-    public static void main(String args[]){
-
-        int arr[] = {38, 27, 43, 10};
-
-        mergeSort(arr, 0, arr.length - 1);
-
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] values = {38, 27, 43, 10};
+
+        sort(values);
+        printArray(values);
     }
 }

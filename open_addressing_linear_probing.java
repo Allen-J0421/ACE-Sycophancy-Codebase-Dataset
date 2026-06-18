d07dcf450f2bd55cba19afe7ec4e71a60b004975
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 12:06:37 -0700

    codex-exp: iteration 6

--- open_addressing_linear_probing.java
+++ open_addressing_linear_probing.java
@@ -161,9 +161,13 @@ class LinearProbingHashMap {
     }
 }
 
-class hashMap extends LinearProbingHashMap {
+class HashMapDemo {
     public static void main(String[] args) {
         LinearProbingHashMap map = new LinearProbingHashMap();
+        runDemo(map);
+    }
+
+    private static void runDemo(LinearProbingHashMap map) {
         map.put(1, 1);
         map.put(2, 2);
         map.put(2, 3);
@@ -173,5 +177,11 @@ class hashMap extends LinearProbingHashMap {
         System.out.println(map.size());
         System.out.println(map.isEmpty());
         System.out.println(map.get(2));
+    }
+}
+
+class hashMap extends LinearProbingHashMap {
+    public static void main(String[] args) {
+        HashMapDemo.main(args);
     }
 }

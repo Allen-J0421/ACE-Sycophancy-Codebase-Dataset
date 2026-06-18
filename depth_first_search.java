607db5b22487ca7147661fe481085b32c7683d9d
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 01:25:07 -0700

    claude-exp: iteration 1

--- depth_first_search.java
+++ depth_first_search.java
@@ -1,55 +1,69 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class DepthFirstSearch {
 
-    private static void
-    dfsRec(ArrayList<ArrayList<Integer> > adj,
-           boolean[] visited, int s, ArrayList<Integer> res) {
-        visited[s] = true;
-        res.add(s);
+    static class Graph {
+        private final int vertices;
+        private final List<List<Integer>> adj;
 
-        for (int i : adj.get(s)) {
-            if (!visited[i]) {
-                dfsRec(adj, visited, i, res);
+        Graph(int vertices) {
+            this.vertices = vertices;
+            this.adj = new ArrayList<>();
+            for (int i = 0; i < vertices; i++) {
+                adj.add(new ArrayList<>());
+            }
+        }
+
+        void addEdge(int u, int v) {
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int size() {
+            return vertices;
+        }
+
+        List<Integer> neighbors(int node) {
+            return adj.get(node);
+        }
+    }
+
+    private static void dfsRec(Graph graph, boolean[] visited, int node, List<Integer> result) {
+        visited[node] = true;
+        result.add(node);
+
+        for (int neighbor : graph.neighbors(node)) {
+            if (!visited[neighbor]) {
+                dfsRec(graph, visited, neighbor, result);
             }
         }
     }
 
-    public static ArrayList<Integer>
-    dfs(ArrayList<ArrayList<Integer> > adj) {
-        boolean[] visited = new boolean[adj.size()];
-        ArrayList<Integer> res = new ArrayList<>();
+    public static List<Integer> dfs(Graph graph) {
+        boolean[] visited = new boolean[graph.size()];
+        List<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < adj.size(); i++) {
+        for (int i = 0; i < graph.size(); i++) {
             if (!visited[i]) {
-                dfsRec(adj, visited, i, res);
+                dfsRec(graph, visited, i, result);
             }
         }
 
-        return res;
-    }
-
-    static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
-        adj.get(u).add(v);
-        adj.get(v).add(u);
+        return result;
     }
 
     public static void main(String[] args) {
-        int V = 6;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        Graph graph = new Graph(6);
+        graph.addEdge(1, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(2, 0);
+        graph.addEdge(5, 4);
 
-        for (int i = 0; i < V; i++)
-            adj.add(new ArrayList<>());
+        List<Integer> result = dfs(graph);
 
-        addEdge(adj, 1, 2);
-        addEdge(adj, 0, 3);
-        addEdge(adj, 2, 0);
-        addEdge(adj, 5, 4);
-
-        ArrayList<Integer> res = dfs(adj);
-
-        for (int num : res) {
-            System.out.print(num + " ");
+        for (int node : result) {
+            System.out.print(node + " ");
         }
     }
 }

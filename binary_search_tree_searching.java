de3a9ebe94e23dd9eebf6eb3109bbf5de6525eaa
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-18 10:05:56 -0700

    claude-exp: iteration 1

--- binary_search_tree_searching.java
+++ binary_search_tree_searching.java
@@ -1,40 +1,75 @@
 class Node {
-    int data;
-    Node left, right;
+    int value;
+    Node left;
+    Node right;
 
-    public Node(int item) {
-        data = item;
-        left = right = null;
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
+
 class BinarySearchTree {
-    static boolean search(Node root, int key) {
-        boolean present = false;
+    private Node root;
 
-        while( root != null ) {
-            if( root.data == key ) {
-                present = true;
-                break;
-            }
-
-            else if( key > root.data )
-                root = root.right;
-
-            else root = root.left;
-        }
-        return present;
+    public BinarySearchTree() {
+        this.root = null;
     }
 
+    public void insert(int value) {
+        root = insertRecursive(root, value);
+    }
+
+    private Node insertRecursive(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = insertRecursive(node.left, value);
+        } else if (value > node.value) {
+            node.right = insertRecursive(node.right, value);
+        }
+        return node;
+    }
+
+    public boolean search(int key) {
+        return searchRecursive(root, key);
+    }
+
+    private boolean searchRecursive(Node node, int key) {
+        if (node == null) {
+            return false;
+        }
+
+        if (key == node.value) {
+            return true;
+        } else if (key < node.value) {
+            return searchRecursive(node.left, key);
+        } else {
+            return searchRecursive(node.right, key);
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
+
+class BinarySearchTreeDemo {
     public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
 
-        Node root = new Node(6);
-        root.left = new Node(2);
-        root.right = new Node(8);
-        root.right.left = new Node(7);
-        root.right.right = new Node(9);
+        bst.insert(6);
+        bst.insert(2);
+        bst.insert(8);
+        bst.insert(7);
+        bst.insert(9);
 
-        int key = 7;
+        int searchKey = 7;
+        boolean found = bst.search(searchKey);
 
-        System.out.println(search(root, key));
+        System.out.println("Search for " + searchKey + ": " + found);
     }
 }

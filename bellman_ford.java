2f811166c3777507cc503a5f7d8359f979977223
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-19 17:35:56 -0700

    codex-exp: iteration 2

--- bellman_ford.java
+++ bellman_ford.java
@@ -5,40 +5,69 @@ class BellmanFord {
     private static final int NEGATIVE_CYCLE = -1;
 
     static int[] bellmanFord(int V, int[][] edges, int src) {
+        Edge[] typedEdges = toEdges(edges);
         int[] dist = new int[V];
         Arrays.fill(dist, INF);
         dist[src] = 0;
 
-        for (int i = 0; i < V - 1; i++) {
-            relaxEdges(dist, edges);
-        }
-
-        return hasNegativeCycle(dist, edges) ? new int[]{NEGATIVE_CYCLE} : dist;
-    }
-
-    private static void relaxEdges(int[] dist, int[][] edges) {
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
-
-            if (dist[u] != INF && dist[u] + wt < dist[v]) {
-                dist[v] = dist[u] + wt;
+        for (int pass = 0; pass < V - 1; pass++) {
+            if (!relaxEdges(dist, typedEdges)) {
+                break;
             }
         }
+
+        return hasNegativeCycle(dist, typedEdges) ? new int[]{NEGATIVE_CYCLE} : dist;
     }
 
-    private static boolean hasNegativeCycle(int[] dist, int[][] edges) {
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
+    private static Edge[] toEdges(int[][] edges) {
+        Edge[] typedEdges = new Edge[edges.length];
+        for (int i = 0; i < edges.length; i++) {
+            typedEdges[i] = new Edge(edges[i][0], edges[i][1], edges[i][2]);
+        }
+        return typedEdges;
+    }
 
-            if (dist[u] != INF && dist[u] + wt < dist[v]) {
+    private static boolean relaxEdges(int[] dist, Edge[] edges) {
+        boolean updated = false;
+        for (Edge edge : edges) {
+            if (relaxEdge(dist, edge)) {
+                updated = true;
+            }
+        }
+        return updated;
+    }
+
+    private static boolean hasNegativeCycle(int[] dist, Edge[] edges) {
+        for (Edge edge : edges) {
+            if (canRelax(dist, edge)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static boolean relaxEdge(int[] dist, Edge edge) {
+        if (!canRelax(dist, edge)) {
+            return false;
+        }
+        dist[edge.v] = dist[edge.u] + edge.wt;
+        return true;
+    }
+
+    private static boolean canRelax(int[] dist, Edge edge) {
+        return dist[edge.u] != INF && dist[edge.u] + edge.wt < dist[edge.v];
+    }
+
+    private static final class Edge {
+        private final int u;
+        private final int v;
+        private final int wt;
+
+        private Edge(int u, int v, int wt) {
+            this.u = u;
+            this.v = v;
+            this.wt = wt;
+        }
     }
 
     public static void main(String[] args) {

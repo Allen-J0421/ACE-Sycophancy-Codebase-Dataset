39a3dfc5d6c602e38f95486ca7eaee8363ed11ee
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-20 01:44:13 -0700

    codex-exp: iteration 1

--- articulation_points.java
+++ articulation_points.java
@@ -1,80 +1,111 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class ArticulationPoints {
+    private static final int ROOT_PARENT = -1;
+    private static final int NO_ARTICULATION_POINTS = -1;
 
-    static ArrayList<ArrayList<Integer>> constructAdj(int V, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+    static ArrayList<ArrayList<Integer>> buildAdjacencyList(int vertexCount, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<>();
+        for (int vertex = 0; vertex < vertexCount; vertex++) {
+            adjacencyList.add(new ArrayList<>());
+        }
 
         for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+            int from = edge[0];
+            int to = edge[1];
+            adjacencyList.get(from).add(to);
+            adjacencyList.get(to).add(from);
         }
-        return adj;
+
+        return adjacencyList;
     }
 
-    static void findPoints(ArrayList<ArrayList<Integer>> adj, int u, int[] visited,
-                                  int[] disc, int[] low,
-                                   int[] time, int parent, int[] isAP) {
+    static ArrayList<Integer> articulationPoints(int vertexCount, ArrayList<ArrayList<Integer>> adjacencyList) {
+        DepthFirstSearchState searchState = new DepthFirstSearchState(vertexCount, adjacencyList);
 
-        visited[u] = 1;
-        disc[u] = low[u] = ++time[0];
-        int children = 0;
-
-        for (int v : adj.get(u)) {
-            if (visited[v] == 0) {
-                children++;
-                findPoints(adj, v, visited, disc, low, time, u, isAP);
-
-                low[u] = Math.min(low[u], low[v]);
-
-                if (parent != -1 && low[v] >= disc[u]) {
-                    isAP[u] = 1;
-                }
-            } else if (v != parent) {
-                low[u] = Math.min(low[u], disc[v]);
+        for (int vertex = 0; vertex < vertexCount; vertex++) {
+            if (!searchState.isVisited(vertex)) {
+                searchState.visit(vertex, ROOT_PARENT);
             }
         }
 
-        if (parent == -1 && children > 1) {
-            isAP[u] = 1;
-        }
-    }
-
-    static ArrayList<Integer> articulationPoints(int V, ArrayList<ArrayList<Integer>> adj) {
-
-        int[] disc = new int[V], low = new int[V],
-        visited = new int[V], isAP = new int[V];
-        int[] time = {0};
-
-        for (int u = 0; u < V; u++) {
-            if (visited[u] == 0) {
-                findPoints(adj, u, visited, disc, low, time, -1, isAP);
-            }
-        }
-
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int u = 0; u < V; u++) {
-            if (isAP[u] == 1) {
-                result.add(u);
-            }
-        }
-
-        if (result.isEmpty()) result.add(-1);
-        return result;
+        return searchState.collectArticulationPoints();
     }
 
     public static void main(String[] args) {
-        int V = 5;
-
+        int vertexCount = 5;
         int[][] edges = {{0, 1}, {1, 4}, {2, 3}, {2, 4}, {3, 4}};
 
-        ArrayList<ArrayList<Integer>> adj = constructAdj(V, edges);
-        ArrayList<Integer> ans = articulationPoints(V, adj);
+        ArrayList<ArrayList<Integer>> adjacencyList = buildAdjacencyList(vertexCount, edges);
+        ArrayList<Integer> articulationPoints = articulationPoints(vertexCount, adjacencyList);
 
-        for (int u : ans) {
-            System.out.print(u + " ");
+        for (int vertex : articulationPoints) {
+            System.out.print(vertex + " ");
         }
         System.out.println();
+    }
+
+    private static final class DepthFirstSearchState {
+        private final List<List<Integer>> adjacencyList;
+        private final int[] discoveryTime;
+        private final int[] lowLink;
+        private final boolean[] visited;
+        private final boolean[] articulationPoint;
+        private int currentTime;
+
+        private DepthFirstSearchState(int vertexCount, ArrayList<ArrayList<Integer>> adjacencyList) {
+            this.adjacencyList = new ArrayList<>(adjacencyList);
+            this.discoveryTime = new int[vertexCount];
+            this.lowLink = new int[vertexCount];
+            this.visited = new boolean[vertexCount];
+            this.articulationPoint = new boolean[vertexCount];
+            this.currentTime = 0;
+        }
+
+        private boolean isVisited(int vertex) {
+            return visited[vertex];
+        }
+
+        private void visit(int vertex, int parent) {
+            visited[vertex] = true;
+            discoveryTime[vertex] = ++currentTime;
+            lowLink[vertex] = discoveryTime[vertex];
+
+            int childCount = 0;
+            for (int neighbor : adjacencyList.get(vertex)) {
+                if (!visited[neighbor]) {
+                    childCount++;
+                    visit(neighbor, vertex);
+                    lowLink[vertex] = Math.min(lowLink[vertex], lowLink[neighbor]);
+
+                    if (parent != ROOT_PARENT && lowLink[neighbor] >= discoveryTime[vertex]) {
+                        articulationPoint[vertex] = true;
+                    }
+                } else if (neighbor != parent) {
+                    lowLink[vertex] = Math.min(lowLink[vertex], discoveryTime[neighbor]);
+                }
+            }
+
+            if (parent == ROOT_PARENT && childCount > 1) {
+                articulationPoint[vertex] = true;
+            }
+        }
+
+        private ArrayList<Integer> collectArticulationPoints() {
+            ArrayList<Integer> result = new ArrayList<>();
+
+            for (int vertex = 0; vertex < articulationPoint.length; vertex++) {
+                if (articulationPoint[vertex]) {
+                    result.add(vertex);
+                }
+            }
+
+            if (result.isEmpty()) {
+                result.add(NO_ARTICULATION_POINTS);
+            }
+
+            return result;
+        }
     }
 }

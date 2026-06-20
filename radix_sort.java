8aac7cf2cdbd28985a0d35fc157c37fc0cf87b37
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-20 07:58:29 -0700

    claude-exp: iteration 1

--- radix_sort.java
+++ radix_sort.java
@@ -1,60 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
-class Radix {
+class RadixSort {
 
-    static int getMax(int arr[], int n)
-    {
-        int mx = arr[0];
-        for (int i = 1; i < n; i++)
-            if (arr[i] > mx)
-                mx = arr[i];
-        return mx;
+    private static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 
-    static void countSort(int arr[], int n, int exp)
-    {
-        int output[] = new int[n];
-        int i;
-        int count[] = new int[10];
-        Arrays.fill(count, 0);
+    private static void countSort(int[] arr, int exp) {
+        int[] output = new int[arr.length];
+        int[] count = new int[10];
 
-        for (i = 0; i < n; i++)
-            count[(arr[i] / exp) % 10]++;
-
-        for (i = 1; i < 10; i++)
-            count[i] += count[i - 1];
-
-        for (i = n - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
+        for (int val : arr) {
+            count[(val / exp) % 10]++;
         }
 
-        for (i = 0; i < n; i++)
-            arr[i] = output[i];
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, arr.length);
     }
 
-    static void radixsort(int arr[], int n)
-    {
-
-        int m = getMax(arr, n);
-
-        for (int exp = 1; m / exp > 0; exp *= 10)
-            countSort(arr, n, exp);
+    static void sort(int[] arr) {
+        int max = getMax(arr);
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, exp);
+        }
     }
 
-    static void print(int arr[], int n)
-    {
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
-    }
-
-    public static void main(String[] args)
-    {
-        int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
-        int n = arr.length;
-
-        radixsort(arr, n);
-        print(arr, n);
+    public static void main(String[] args) {
+        int[] arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }

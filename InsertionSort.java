6353ef5b780f758525edc2440990481c2ebc1400
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-20 05:01:56 -0700

    codex-exp: iteration 1

---
+++ InsertionSort.java
@@ -0,0 +1,34 @@
+public final class InsertionSort {
+
+    private InsertionSort() {
+        // Utility class.
+    }
+
+    private static void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+
+        sort(array);
+        printArray(array);
+    }
+}

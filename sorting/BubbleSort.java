a5bb321f4405f6ff4eb5cd48c444207cea673a84
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-20 09:27:25 -0700

    claude-exp: iteration 3

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -5,10 +5,9 @@ public class BubbleSort {
     private BubbleSort() {}
 
     public static void sort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                     swapped = true;

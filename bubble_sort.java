302ec66bc5bca4c431b7b73fcb98bccded50cad7
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-20 09:02:21 -0700

    codex-exp: iteration 5

--- bubble_sort.java
+++ bubble_sort.java
@@ -8,19 +8,23 @@ class BubbleSort {
 
     static void bubbleSort(int[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j + 1);
-                    swapped = true;
-                }
-            }
-
-            if (!swapped) {
+            if (!bubblePass(arr, n - i)) {
                 break;
             }
         }
+    }
+
+    private static boolean bubblePass(int[] arr, int unsortedLength) {
+        boolean swapped = false;
+
+        for (int j = 0; j < unsortedLength - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                swapped = true;
+            }
+        }
+
+        return swapped;
     }
 
     private static void swap(int[] arr, int firstIndex, int secondIndex) {

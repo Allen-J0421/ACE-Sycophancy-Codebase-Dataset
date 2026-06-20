2a5cef7b97634948393cf8e9c40f5851a5a17705
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-19 20:19:02 -0700

    claude-exp: iteration 5

--- kruskals_mst.java
+++ kruskals_mst.java
@@ -52,10 +52,7 @@ class KruskalMST {
         UnionFind(int n) {
             parent = new int[n];
             rank = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 1;
-            }
+            Arrays.setAll(parent, i -> i);
         }
 
         int find(int i) {

4aca391d316385899699056d96b6a4f7e09a740d
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-20 05:06:13 -0700

    codex-exp: iteration 5

--- InsertionSort.java
+++ InsertionSort.java
@@ -13,7 +13,7 @@ public final class InsertionSort {
 
     public static void sort(int[] array, int fromIndex, int toIndex) {
         Objects.requireNonNull(array, "array");
-        checkRange(array.length, fromIndex, toIndex);
+        Objects.checkFromToIndex(fromIndex, toIndex, array.length);
 
         for (int i = fromIndex + 1; i < toIndex; i++) {
             int key = array[i];
@@ -35,24 +35,46 @@ public final class InsertionSort {
 
     public static int[] sortedCopy(int[] array, int fromIndex, int toIndex) {
         Objects.requireNonNull(array, "array");
-        checkRange(array.length, fromIndex, toIndex);
+        Objects.checkFromToIndex(fromIndex, toIndex, array.length);
 
         int[] copy = array.clone();
         sort(copy, fromIndex, toIndex);
         return copy;
     }
 
-    private static void checkRange(int length, int fromIndex, int toIndex) {
-        if (fromIndex < 0) {
-            throw new IndexOutOfBoundsException("fromIndex (" + fromIndex + ") is negative");
+    public static <T extends Comparable<? super T>> void sort(T[] array) {
+        Objects.requireNonNull(array, "array");
+        sort(array, 0, array.length);
+    }
+
+    public static <T extends Comparable<? super T>> void sort(T[] array, int fromIndex, int toIndex) {
+        Objects.requireNonNull(array, "array");
+        Objects.checkFromToIndex(fromIndex, toIndex, array.length);
+
+        for (int i = fromIndex + 1; i < toIndex; i++) {
+            T key = array[i];
+            int j = i - 1;
+
+            while (j >= fromIndex && array[j].compareTo(key) > 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
         }
-        if (toIndex > length) {
-            throw new IndexOutOfBoundsException(
-                    "toIndex (" + toIndex + ") is greater than array length (" + length + ")");
-        }
-        if (fromIndex > toIndex) {
-            throw new IllegalArgumentException(
-                    "fromIndex (" + fromIndex + ") is greater than toIndex (" + toIndex + ")");
-        }
+    }
+
+    public static <T extends Comparable<? super T>> T[] sortedCopy(T[] array) {
+        Objects.requireNonNull(array, "array");
+        return sortedCopy(array, 0, array.length);
+    }
+
+    public static <T extends Comparable<? super T>> T[] sortedCopy(T[] array, int fromIndex, int toIndex) {
+        Objects.requireNonNull(array, "array");
+        Objects.checkFromToIndex(fromIndex, toIndex, array.length);
+
+        T[] copy = array.clone();
+        sort(copy, fromIndex, toIndex);
+        return copy;
     }
 }

18ecd3a629d6501a7a22917b31e88281c8a8dbb0
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-19 18:44:49 -0700

    codex-exp: iteration 9

--- ExampleGraphs.java
+++ ExampleGraphs.java
@@ -1,28 +1,12 @@
 final class ExampleGraphs {
-    private static final int INF = FloydWarshall.NO_PATH;
-    private static final int[][] WEIGHTED_DIRECTED_GRAPH = {
-        {0, 4, INF, 5, INF},
-        {INF, 0, 1, INF, 6},
-        {2, INF, 0, 3, INF},
-        {INF, INF, 1, 0, 2},
-        {1, INF, INF, 4, 0}
-    };
-    private static final int[][] WEIGHTED_DIRECTED_GRAPH_SHORTEST_PATHS = {
-        {0, 4, 5, 5, 7},
-        {3, 0, 1, 4, 6},
-        {2, 6, 0, 3, 5},
-        {3, 7, 1, 0, 2},
-        {1, 5, 5, 4, 0}
-    };
-
     private ExampleGraphs() {
     }
 
     static int[][] weightedDirectedGraph() {
-        return MatrixUtils.copyRows(WEIGHTED_DIRECTED_GRAPH);
+        return GraphFixtures.weightedDirectedGraph();
     }
 
     static int[][] weightedDirectedGraphShortestPaths() {
-        return MatrixUtils.copyRows(WEIGHTED_DIRECTED_GRAPH_SHORTEST_PATHS);
+        return GraphFixtures.weightedDirectedGraphShortestPaths();
     }
 }
--- TestGraphs.java
+++ TestGraphs.java
@@ -1,64 +1,32 @@
 final class TestGraphs {
-    private static final int INF = FloydWarshall.NO_PATH;
-    private static final int[][] ACYCLIC_THREE_NODE_GRAPH = {
-        {0, 2, INF},
-        {INF, 0, 3},
-        {INF, INF, 0}
-    };
-    private static final int[][] NEGATIVE_CYCLE_GRAPH = {
-        {0, 1, INF},
-        {INF, 0, -2},
-        {-2, INF, 0}
-    };
-    private static final int[][] OVERFLOW_GRAPH = {
-        {0, Integer.MAX_VALUE - 1, INF},
-        {INF, 0, 2},
-        {INF, INF, 0}
-    };
-    private static final int[][] UNREACHABLE_TWO_NODE_DISTANCES = {
-        {0, INF},
-        {INF, 0}
-    };
-    private static final int[][] INVALID_RECTANGULAR_MATRIX = {
-        {0},
-        {1, 0}
-    };
-    private static final int[][] MATRIX_WITH_NULL_ROW = {
-        {0},
-        null
-    };
-    private static final int[][] NON_SQUARE_ROW_MATRIX = {
-        {0, 1}
-    };
-
     private TestGraphs() {
     }
 
     static int[][] acyclicThreeNodeGraph() {
-        return MatrixUtils.copyRows(ACYCLIC_THREE_NODE_GRAPH);
+        return GraphFixtures.acyclicThreeNodeGraph();
     }
 
     static int[][] negativeCycleGraph() {
-        return MatrixUtils.copyRows(NEGATIVE_CYCLE_GRAPH);
+        return GraphFixtures.negativeCycleGraph();
     }
 
     static int[][] overflowGraph() {
-        return MatrixUtils.copyRows(OVERFLOW_GRAPH);
+        return GraphFixtures.overflowGraph();
     }
 
     static int[][] unreachableTwoNodeDistances() {
-        return MatrixUtils.copyRows(UNREACHABLE_TWO_NODE_DISTANCES);
+        return GraphFixtures.unreachableTwoNodeDistances();
     }
 
     static int[][] invalidRectangularMatrix() {
-        return MatrixUtils.copyRows(INVALID_RECTANGULAR_MATRIX);
+        return GraphFixtures.invalidRectangularMatrix();
     }
 
     static int[][] matrixWithNullRow() {
-        return MatrixUtils.copyRows(MATRIX_WITH_NULL_ROW);
+        return GraphFixtures.matrixWithNullRow();
     }
 
     static int[][] nonSquareRowMatrix() {
-        return MatrixUtils.copyRows(NON_SQUARE_ROW_MATRIX);
+        return GraphFixtures.nonSquareRowMatrix();
     }
 }

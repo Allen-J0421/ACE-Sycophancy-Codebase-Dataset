b149bb33f95695d2f027c4c7919ee684587b3270
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-25 18:48:37 -0700

    codex-exp: iteration 1

--- modular_exponentiation.java
+++ modular_exponentiation.java
@@ -1,26 +1,35 @@
-class ModularExponentiation {
-    public int powMod(int x, int n, int M) {
-        int res = 1;
+final class ModularExponentiation {
+    private ModularExponentiation() {
+        // Utility class.
+    }
 
-        while (n >= 1) {
+    public static int powMod(int base, int exponent, int modulus) {
+        int result = 1;
+        int currentBase = base;
+        int currentExponent = exponent;
 
-            if ((n & 1) == 1) {
-                res = (int)((1L * res * x) % M);
-
-                n--;
-             } else {
-
-                x = (int)((1L * x * x) % M);
-                n /= 2;
+        while (currentExponent > 0) {
+            if ((currentExponent & 1) == 1) {
+                result = multiplyMod(result, currentBase, modulus);
+                currentExponent--;
+            } else {
+                currentBase = multiplyMod(currentBase, currentBase, modulus);
+                currentExponent /= 2;
             }
         }
 
-        return res;
+        return result;
+    }
+
+    private static int multiplyMod(int left, int right, int modulus) {
+        return (int) ((1L * left * right) % modulus);
     }
 
     public static void main(String[] args) {
-        int x = 3, n = 2, M = 4;
-        ModularExponentiation obj = new ModularExponentiation();
-        System.out.println(obj.powMod(x, n, M));
+        int base = 3;
+        int exponent = 2;
+        int modulus = 4;
+
+        System.out.println(powMod(base, exponent, modulus));
     }
 }

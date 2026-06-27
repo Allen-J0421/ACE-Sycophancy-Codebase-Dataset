eddfe1426dc566ccd2108a5863fd99cb49bc1c30
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-27 15:44:40 -0700

    codex-exp: iteration 3

--- Grass.java
+++ Grass.java
@@ -1,122 +1,65 @@
-import java.util.List;
-import java.util.Random;
-
 /**
  * A simple model of grass - a type of plant.
- *
- * @version 2022.02.28
  */
 public class Grass extends Plant
 {
-    // Characteristics shared by all grass (class variables).
-
-    // The age to which a grass can live.
     private static final int MAX_AGE = 20;
-    // The likelihood of a grass reproducing.
     private static final double REPRODUCTION_PROBABILITY = 0.44;
-    // The maximum number of times it can reproduce.
     private static final int MAX_LITTER_SIZE = 6;
-    // The food value of a single grass, which the predator gets when they eat grass. 
-    //In effect, this is the number of steps a predator of grass can go before it has to eat again.
+    private static final int REPRODUCTION_WATER_THRESHOLD = 9;
+    private static final int INITIAL_WATER_UPPER_BOUND = 15;
     private static final int GRASS_FOOD_VALUE = 10;
-    // A shared random number generator to control reproduction of grass.
-    private static final Random rand = Randomizer.getRandom();
-    
-    /**
-     * Create a piece of gras. Grass can be created as a new born (age zero) or with a random age and food level.
-     * 
-     * @param randomAge If true, the age will have random age and water level.
-     * @param field The field currently occupied.
-     * @param location The location within the field.
-     */
+
     public Grass(boolean randomAge, Field field, Location location)
     {
         super(randomAge, field, location);
-        this.setAge(0);
-        if(randomAge) {
-            //random age & water level assigned to plant
-            this.setAge(rand.nextInt(MAX_AGE));
-            setWaterLevel(rand.nextInt(15));
-        }
-    }
-    
-    /**
-     * This is what the grass does each step of the simulation 
-     * - it grows and may reproduce.
-     * @param newGrass A list to return newly grown grass
-     */
-    @Override
-    public void act(List<Actor> newGrass)
-    {
-        super.act(newGrass);
-        if(isAlive()) {
-            giveBirth(newGrass); 
-            findWater();
-        }
     }
 
-    /**
-     * Increase the age.
-     * This could result in the plant's death if they have reached their
-     * maximum age.
-     */
-    @Override
-    public void incrementAge()
-    {
-        super.incrementAge();
-        if(this.getAge() > MAX_AGE) {
-            setDead();
-        }
-    }
-    
-    /**
-     * Check whether or not this plant is to reproduce at this step.
-     * New growths will be made into free adjacent locations.
-     * @param newGrass A list to return newly grown grass
-     */
-    private void giveBirth(List<Actor> newGrass)
-    {
-        // New mice are born into adjacent locations.
-        // Get a list of adjacent free locations.
-        Field field = getField();
-        List<Location> free = field.getFreeAdjacentLocations(getLocation());
-        int births = reproduce();
-        for(int b = 0; b < births && free.size() > 0; b++) {
-            Location loc = free.remove(0);
-            Grass young = new Grass(false, field, loc);
-            newGrass.add(young);
-        }
-    }
-        
-    /**
-     * Generate a number representing the number of births,
-     * if it can reproduce.
-     * @return The number of births (may be zero).
-     */
-    private int reproduce()
-    {
-        int births = 0;
-        if(canReproduce() && rand.nextDouble() <= REPRODUCTION_PROBABILITY) {
-            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
-        }
-        return births;
-    }
-
-    /**
-     * A grass can reproduce if it's water level is greater than 9.
-     * @return true if the plant can reproduce, false otherwise.
-     */
-    private boolean canReproduce()
-    {
-        return (this.getWaterLevel() >= 9);
-    }
-    
-    /**
-     * @return GRASS_FOOD_VALUE The food value of grass.
-     */
     @Override
     public int getFoodValue()
-    { 
+    {
         return GRASS_FOOD_VALUE;
+    }
+
+    @Override
+    protected int getMaxAge()
+    {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected double getReproductionProbability()
+    {
+        return REPRODUCTION_PROBABILITY;
+    }
+
+    @Override
+    protected int getMaxLitterSize()
+    {
+        return MAX_LITTER_SIZE;
+    }
+
+    @Override
+    protected int getReproductionWaterThreshold()
+    {
+        return REPRODUCTION_WATER_THRESHOLD;
+    }
+
+    @Override
+    protected int getStartingWaterLevelUpperBound()
+    {
+        return INITIAL_WATER_UPPER_BOUND;
+    }
+
+    @Override
+    protected int getWaterSearchThreshold()
+    {
+        return 5;
+    }
+
+    @Override
+    protected Plant createOffspring(Field field, Location location)
+    {
+        return new Grass(false, field, location);
     }
 }
--- Plant.java
+++ Plant.java
@@ -2,12 +2,15 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * A class representing shared characteristics of plants
+ * A class representing shared characteristics of plants.
  *
  * @version 2022.02.28
  */
 public abstract class Plant extends Organism
 {
+    // Shared random source for plant lifecycle decisions.
+    private static final Random rand = Randomizer.getRandom();
+
     /**
      * Create a new plant at location in field.
      * @param randomAge If true, the plant will have a random age assigned to it.
@@ -17,29 +20,129 @@ public abstract class Plant extends Organism
     public Plant(boolean randomAge, Field field, Location location)
     {
         super(randomAge, field, location);
-        Random rand = Randomizer.getRandom();
-        if(!randomAge) {
-            setAge(0);
-            //sets water level to a random value when first created
-            setWaterLevel(rand.nextInt(15));
+        setAge(0);
+        setWaterLevel(rand.nextInt(getStartingWaterLevelUpperBound()));
+        if(randomAge) {
+            setAge(rand.nextInt(getMaxAge()));
         }
     }
-    
+
     /**
-     * Make this plant act - that is: make it do
-     * whatever it needs to do.
-     * @param newPlants A list to receive newly grown plants
+     * Make this plant act - that is: make it do whatever it needs to do.
+     * @param newPlants A list to receive newly grown plants.
      */
+    @Override
     public void act(List<Actor> newPlants)
     {
         super.act(newPlants);
+        if (isAlive()) {
+            giveBirth(newPlants);
+            findWater();
+        }
     }
-    
+
     /**
-     * Look for water adjacent to the plant's current location
-     * @return Where water was found, or null if it wasn't
+     * Increase the age and kill the plant when it exceeds its lifespan.
      */
-    protected Location findWater() {
-        return getWaterLevel() <= 5 ? super.findWater() : null;
+    @Override
+    public void incrementAge()
+    {
+        super.incrementAge();
+        if (getAge() > getMaxAge()) {
+            setDead();
+        }
     }
+
+    /**
+     * Look for water adjacent to the plant's current location.
+     * @return Where water was found, or null if it wasn't.
+     */
+    protected Location findWater()
+    {
+        return getWaterLevel() <= getWaterSearchThreshold() ? super.findWater() : null;
+    }
+
+    /**
+     * Check whether or not this plant is to reproduce at this step.
+     * New growths will be made into free adjacent locations.
+     * @param newPlants A list to return newly grown plants.
+     * @return The free adjacent locations for the new plants, or null if reproduction is not possible.
+     */
+    protected List<Location> giveBirth(List<Actor> newPlants)
+    {
+        int births = reproduce();
+        if (births <= 0) {
+            return null;
+        }
+
+        List<Location> free = getField().getFreeAdjacentLocations(getLocation());
+        if (free.isEmpty()) {
+            return null;
+        }
+
+        Field field = getField();
+        for(int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+            Plant young = createOffspring(field, loc);
+            newPlants.add(young);
+        }
+        return null;
+    }
+
+    /**
+     * Determine whether this plant can currently reproduce.
+     */
+    private boolean canReproduce()
+    {
+        return getWaterLevel() >= getReproductionWaterThreshold();
+    }
+
+    /**
+     * Generate the number of offspring produced by this plant.
+     */
+    private int reproduce()
+    {
+        if (canReproduce() && rand.nextDouble() <= getReproductionProbability()) {
+            return rand.nextInt(getMaxLitterSize()) + 1;
+        }
+        return 0;
+    }
+
+    /**
+     * @return The age at which this plant can die of old age.
+     */
+    protected abstract int getMaxAge();
+
+    /**
+     * @return The probability that this plant reproduces when conditions are met.
+     */
+    protected abstract double getReproductionProbability();
+
+    /**
+     * @return The maximum number of offspring this plant can produce at once.
+     */
+    protected abstract int getMaxLitterSize();
+
+    /**
+     * @return The minimum water level required to reproduce.
+     */
+    protected abstract int getReproductionWaterThreshold();
+
+    /**
+     * @return The upper bound used for initial water level generation.
+     */
+    protected abstract int getStartingWaterLevelUpperBound();
+
+    /**
+     * @return The water level at which the plant starts seeking water.
+     */
+    protected abstract int getWaterSearchThreshold();
+
+    /**
+     * Create a new offspring of the current plant species.
+     * @param field The field the offspring should occupy.
+     * @param location The offspring's location.
+     * @return A new instance of the same plant species.
+     */
+    protected abstract Plant createOffspring(Field field, Location location);
 }

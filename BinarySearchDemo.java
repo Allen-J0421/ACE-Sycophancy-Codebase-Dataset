b7a28a49a4691652b538a2c42faaed80f300d69f
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 13:33:00 -0700

    codex-exp: iteration 10

--- BinarySearchDemo.java
+++ BinarySearchDemo.java
@@ -1,11 +1,12 @@
 public final class BinarySearchDemo {
+    private static final int[] SAMPLE_VALUES = { 2, 3, 4, 10, 40 };
+    private static final int SAMPLE_TARGET = 10;
+
     private BinarySearchDemo() {
     }
 
     public static void main(String[] args) {
-        int[] sortedValues = { 2, 3, 4, 10, 40 };
-        int target = 10;
-        int result = BinarySearch.indexOf(sortedValues, target);
+        int result = BinarySearch.indexOf(SAMPLE_VALUES, SAMPLE_TARGET);
 
         System.out.println(formatSearchResult(result));
     }

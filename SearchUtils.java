f02c66c31c7174fe3033c103ba2d7319608e62c4
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 15:52:05 -0700

    codex-exp: iteration 1

---
+++ SearchUtils.java
@@ -0,0 +1,22 @@
+public final class SearchUtils {
+    private SearchUtils() {
+    }
+
+    public static <T extends Comparable<? super T>> int binarySearch(T[] arr, T target) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int comparison = arr[mid].compareTo(target);
+
+            if (comparison == 0)
+                return mid;
+
+            if (comparison < 0)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+}

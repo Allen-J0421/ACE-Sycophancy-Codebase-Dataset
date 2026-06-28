add725a19db50695088cba2bfe5900832c8d36ed
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 06:41:10 -0700

    codex-exp: iteration 4

---
+++ guava/src/com/google/common/collect/MapsAsMapViews.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright (C) 2026 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.collect;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Collections2.safeContains;
+import static com.google.common.collect.Collections2.transform;
+
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.base.Function;
+import com.google.j2objc.annotations.WeakOuter;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.Spliterator;
+import java.util.function.BiConsumer;
+import org.jspecify.annotations.Nullable;
+
+class AsMapView<K extends @Nullable Object, V extends @Nullable Object>
+    extends Maps.ViewCachingAbstractMap<K, V> {
+
+  private final Set<K> set;
+  final Function<? super K, V> function;
+
+  AsMapView(Set<K> set, Function<? super K, V> function) {
+    this.set = checkNotNull(set);
+    this.function = checkNotNull(function);
+  }
+
+  Set<K> backingSet() {
+    return set;
+  }
+
+  @Override
+  Set<K> createKeySet() {
+    return Maps.removeOnlySet(backingSet());
+  }
+
+  @Override
+  Collection<V> createValues() {
+    return transform(set, function);
+  }
+
+  @Override
+  public int size() {
+    return backingSet().size();
+  }
+
+  @Override
+  public boolean containsKey(@Nullable Object key) {
+    return backingSet().contains(key);
+  }
+
+  @Override
+  public @Nullable V get(@Nullable Object key) {
+    return getOrDefault(key, null);
+  }
+
+  @Override
+  public @Nullable V getOrDefault(@Nullable Object key, @Nullable V defaultValue) {
+    if (safeContains(backingSet(), key)) {
+      @SuppressWarnings("unchecked") // unsafe, but Javadoc warns about it
+      K k = (K) key;
+      return function.apply(k);
+    }
+    return defaultValue;
+  }
+
+  @Override
+  public @Nullable V remove(@Nullable Object key) {
+    if (backingSet().remove(key)) {
+      @SuppressWarnings("unchecked") // unsafe, but Javadoc warns about it
+      K k = (K) key;
+      return function.apply(k);
+    }
+    return null;
+  }
+
+  @Override
+  public void clear() {
+    backingSet().clear();
+  }
+
+  @Override
+  Set<Entry<K, V>> createEntrySet() {
+    @WeakOuter
+    final class EntrySetImpl extends Maps.EntrySet<K, V> {
+      @Override
+      Map<K, V> map() {
+        return AsMapView.this;
+      }
+
+      @Override
+      public Iterator<Entry<K, V>> iterator() {
+        return asMapEntryIterator(backingSet(), function);
+      }
+    }
+    return new EntrySetImpl();
+  }
+
+  @Override
+  public void forEach(BiConsumer<? super K, ? super V> action) {
+    checkNotNull(action);
+    // avoids allocation of entries
+    backingSet().forEach(k -> action.accept(k, function.apply(k)));
+  }
+
+  static <K extends @Nullable Object, V extends @Nullable Object> Iterator<Entry<K, V>>
+      asMapEntryIterator(Set<K> set, Function<? super K, V> function) {
+    return new TransformedIterator<K, Entry<K, V>>(set.iterator()) {
+      @Override
+      Entry<K, V> transform(@ParametricNullness K key) {
+        return Maps.immutableEntry(key, function.apply(key));
+      }
+    };
+  }
+}
+
+final class SortedAsMapView<K extends @Nullable Object, V extends @Nullable Object>
+    extends AsMapView<K, V> implements SortedMap<K, V> {
+
+  SortedAsMapView(SortedSet<K> set, Function<? super K, V> function) {
+    super(set, function);
+  }
+
+  @Override
+  SortedSet<K> backingSet() {
+    return (SortedSet<K>) super.backingSet();
+  }
+
+  @Override
+  public @Nullable Comparator<? super K> comparator() {
+    return backingSet().comparator();
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return Maps.removeOnlySortedSet(backingSet());
+  }
+
+  @Override
+  public SortedMap<K, V> subMap(@ParametricNullness K fromKey, @ParametricNullness K toKey) {
+    return Maps.asMap(backingSet().subSet(fromKey, toKey), function);
+  }
+
+  @Override
+  public SortedMap<K, V> headMap(@ParametricNullness K toKey) {
+    return Maps.asMap(backingSet().headSet(toKey), function);
+  }
+
+  @Override
+  public SortedMap<K, V> tailMap(@ParametricNullness K fromKey) {
+    return Maps.asMap(backingSet().tailSet(fromKey), function);
+  }
+
+  @Override
+  @ParametricNullness
+  public K firstKey() {
+    return backingSet().first();
+  }
+
+  @Override
+  @ParametricNullness
+  public K lastKey() {
+    return backingSet().last();
+  }
+}
+
+@GwtIncompatible // NavigableMap
+final class NavigableAsMapView<K extends @Nullable Object, V extends @Nullable Object>
+    extends AbstractNavigableMap<K, V> {
+  /*
+   * Using AbstractNavigableMap is simpler than extending SortedAsMapView and rewriting all the
+   * NavigableMap methods.
+   */
+
+  private final NavigableSet<K> set;
+  private final Function<? super K, V> function;
+
+  NavigableAsMapView(NavigableSet<K> set, Function<? super K, V> function) {
+    this.set = checkNotNull(set);
+    this.function = checkNotNull(function);
+  }
+
+  @Override
+  public NavigableMap<K, V> subMap(
+      @ParametricNullness K fromKey,
+      boolean fromInclusive,
+      @ParametricNullness K toKey,
+      boolean toInclusive) {
+    return Maps.asMap(set.subSet(fromKey, fromInclusive, toKey, toInclusive), function);
+  }
+
+  @Override
+  public NavigableMap<K, V> headMap(@ParametricNullness K toKey, boolean inclusive) {
+    return Maps.asMap(set.headSet(toKey, inclusive), function);
+  }
+
+  @Override
+  public NavigableMap<K, V> tailMap(@ParametricNullness K fromKey, boolean inclusive) {
+    return Maps.asMap(set.tailSet(fromKey, inclusive), function);
+  }
+
+  @Override
+  public @Nullable Comparator<? super K> comparator() {
+    return set.comparator();
+  }
+
+  @Override
+  public @Nullable V get(@Nullable Object key) {
+    return getOrDefault(key, null);
+  }
+
+  @Override
+  public @Nullable V getOrDefault(@Nullable Object key, @Nullable V defaultValue) {
+    if (safeContains(set, key)) {
+      @SuppressWarnings("unchecked") // unsafe, but Javadoc warns about it
+      K k = (K) key;
+      return function.apply(k);
+    }
+    return defaultValue;
+  }
+
+  @Override
+  public void clear() {
+    set.clear();
+  }
+
+  @Override
+  Iterator<Entry<K, V>> entryIterator() {
+    return AsMapView.asMapEntryIterator(set, function);
+  }
+
+  @Override
+  Spliterator<Entry<K, V>> entrySpliterator() {
+    return CollectSpliterators.map(
+        set.spliterator(),
+        Spliterator.DISTINCT | Spliterator.NONNULL,
+        e -> Maps.immutableEntry(e, function.apply(e)));
+  }
+
+  @Override
+  public void forEach(BiConsumer<? super K, ? super V> action) {
+    set.forEach(k -> action.accept(k, function.apply(k)));
+  }
+
+  @Override
+  Iterator<Entry<K, V>> descendingEntryIterator() {
+    return descendingMap().entrySet().iterator();
+  }
+
+  @Override
+  public NavigableSet<K> navigableKeySet() {
+    return Maps.removeOnlyNavigableSet(set);
+  }
+
+  @Override
+  public int size() {
+    return set.size();
+  }
+
+  @Override
+  public NavigableMap<K, V> descendingMap() {
+    return Maps.asMap(set.descendingSet(), function);
+  }
+}
---
+++ guava/src/com/google/common/collect/MapsTransformedEntriesMaps.java
@@ -0,0 +1,297 @@
+/*
+ * Copyright (C) 2026 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.collect;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Iterators.transform;
+import static com.google.common.collect.NullnessCasts.uncheckedCastNullableTToT;
+
+import com.google.common.annotations.GwtIncompatible;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.Spliterator;
+import java.util.function.BiConsumer;
+import org.jspecify.annotations.Nullable;
+
+class TransformedEntriesMap<
+        K extends @Nullable Object, V1 extends @Nullable Object, V2 extends @Nullable Object>
+    extends Maps.IteratorBasedAbstractMap<K, V2> {
+  final Map<K, V1> fromMap;
+  final Maps.EntryTransformer<? super K, ? super V1, V2> transformer;
+
+  TransformedEntriesMap(
+      Map<K, V1> fromMap, Maps.EntryTransformer<? super K, ? super V1, V2> transformer) {
+    this.fromMap = checkNotNull(fromMap);
+    this.transformer = checkNotNull(transformer);
+  }
+
+  @Override
+  public int size() {
+    return fromMap.size();
+  }
+
+  @Override
+  public boolean containsKey(@Nullable Object key) {
+    return fromMap.containsKey(key);
+  }
+
+  @Override
+  public @Nullable V2 get(@Nullable Object key) {
+    return getOrDefault(key, null);
+  }
+
+  // safe as long as the user followed the <b>Warning</b> in the javadoc
+  @SuppressWarnings("unchecked")
+  @Override
+  public @Nullable V2 getOrDefault(@Nullable Object key, @Nullable V2 defaultValue) {
+    V1 value = fromMap.get(key);
+    if (value != null || fromMap.containsKey(key)) {
+      // The cast is safe because of the containsKey check.
+      return transformer.transformEntry((K) key, uncheckedCastNullableTToT(value));
+    }
+    return defaultValue;
+  }
+
+  // safe as long as the user followed the <b>Warning</b> in the javadoc
+  @SuppressWarnings("unchecked")
+  @Override
+  public @Nullable V2 remove(@Nullable Object key) {
+    return fromMap.containsKey(key)
+        // The cast is safe because of the containsKey check.
+        ? transformer.transformEntry((K) key, uncheckedCastNullableTToT(fromMap.remove(key)))
+        : null;
+  }
+
+  @Override
+  public void clear() {
+    fromMap.clear();
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return fromMap.keySet();
+  }
+
+  @Override
+  Iterator<Entry<K, V2>> entryIterator() {
+    return transform(fromMap.entrySet().iterator(), Maps.asEntryToEntryFunction(transformer));
+  }
+
+  @Override
+  @GwtIncompatible // Spliterator
+  Spliterator<Entry<K, V2>> entrySpliterator() {
+    return CollectSpliterators.map(
+        fromMap.entrySet().spliterator(),
+        Spliterator.DISTINCT | Spliterator.NONNULL,
+        Maps.asEntryToEntryFunction(transformer));
+  }
+
+  @Override
+  public void forEach(BiConsumer<? super K, ? super V2> action) {
+    checkNotNull(action);
+    // avoids creating new Entry<K, V2> objects
+    fromMap.forEach((k, v1) -> action.accept(k, transformer.transformEntry(k, v1)));
+  }
+
+  @Override
+  public Collection<V2> values() {
+    return new Maps.Values<>(this);
+  }
+}
+
+class TransformedEntriesSortedMap<
+        K extends @Nullable Object, V1 extends @Nullable Object, V2 extends @Nullable Object>
+    extends TransformedEntriesMap<K, V1, V2> implements SortedMap<K, V2> {
+
+  TransformedEntriesSortedMap(
+      SortedMap<K, V1> fromMap, Maps.EntryTransformer<? super K, ? super V1, V2> transformer) {
+    super(fromMap, transformer);
+  }
+
+  SortedMap<K, V1> fromMap() {
+    return (SortedMap<K, V1>) fromMap;
+  }
+
+  @Override
+  public @Nullable Comparator<? super K> comparator() {
+    return fromMap().comparator();
+  }
+
+  @Override
+  @ParametricNullness
+  public K firstKey() {
+    return fromMap().firstKey();
+  }
+
+  @Override
+  public SortedMap<K, V2> headMap(@ParametricNullness K toKey) {
+    return Maps.transformEntries(fromMap().headMap(toKey), transformer);
+  }
+
+  @Override
+  @ParametricNullness
+  public K lastKey() {
+    return fromMap().lastKey();
+  }
+
+  @Override
+  public SortedMap<K, V2> subMap(@ParametricNullness K fromKey, @ParametricNullness K toKey) {
+    return Maps.transformEntries(fromMap().subMap(fromKey, toKey), transformer);
+  }
+
+  @Override
+  public SortedMap<K, V2> tailMap(@ParametricNullness K fromKey) {
+    return Maps.transformEntries(fromMap().tailMap(fromKey), transformer);
+  }
+}
+
+@GwtIncompatible // NavigableMap
+final class TransformedEntriesNavigableMap<
+        K extends @Nullable Object, V1 extends @Nullable Object, V2 extends @Nullable Object>
+    extends TransformedEntriesSortedMap<K, V1, V2> implements NavigableMap<K, V2> {
+
+  TransformedEntriesNavigableMap(
+      NavigableMap<K, V1> fromMap, Maps.EntryTransformer<? super K, ? super V1, V2> transformer) {
+    super(fromMap, transformer);
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> ceilingEntry(@ParametricNullness K key) {
+    return transformEntry(fromMap().ceilingEntry(key));
+  }
+
+  @Override
+  public @Nullable K ceilingKey(@ParametricNullness K key) {
+    return fromMap().ceilingKey(key);
+  }
+
+  @Override
+  public NavigableSet<K> descendingKeySet() {
+    return fromMap().descendingKeySet();
+  }
+
+  @Override
+  public NavigableMap<K, V2> descendingMap() {
+    return Maps.transformEntries(fromMap().descendingMap(), transformer);
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> firstEntry() {
+    return transformEntry(fromMap().firstEntry());
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> floorEntry(@ParametricNullness K key) {
+    return transformEntry(fromMap().floorEntry(key));
+  }
+
+  @Override
+  public @Nullable K floorKey(@ParametricNullness K key) {
+    return fromMap().floorKey(key);
+  }
+
+  @Override
+  public NavigableMap<K, V2> headMap(@ParametricNullness K toKey) {
+    return headMap(toKey, false);
+  }
+
+  @Override
+  public NavigableMap<K, V2> headMap(@ParametricNullness K toKey, boolean inclusive) {
+    return Maps.transformEntries(fromMap().headMap(toKey, inclusive), transformer);
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> higherEntry(@ParametricNullness K key) {
+    return transformEntry(fromMap().higherEntry(key));
+  }
+
+  @Override
+  public @Nullable K higherKey(@ParametricNullness K key) {
+    return fromMap().higherKey(key);
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> lastEntry() {
+    return transformEntry(fromMap().lastEntry());
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> lowerEntry(@ParametricNullness K key) {
+    return transformEntry(fromMap().lowerEntry(key));
+  }
+
+  @Override
+  public @Nullable K lowerKey(@ParametricNullness K key) {
+    return fromMap().lowerKey(key);
+  }
+
+  @Override
+  public NavigableSet<K> navigableKeySet() {
+    return fromMap().navigableKeySet();
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> pollFirstEntry() {
+    return transformEntry(fromMap().pollFirstEntry());
+  }
+
+  @Override
+  public @Nullable Entry<K, V2> pollLastEntry() {
+    return transformEntry(fromMap().pollLastEntry());
+  }
+
+  @Override
+  public NavigableMap<K, V2> subMap(
+      @ParametricNullness K fromKey,
+      boolean fromInclusive,
+      @ParametricNullness K toKey,
+      boolean toInclusive) {
+    return Maps.transformEntries(
+        fromMap().subMap(fromKey, fromInclusive, toKey, toInclusive), transformer);
+  }
+
+  @Override
+  public NavigableMap<K, V2> subMap(@ParametricNullness K fromKey, @ParametricNullness K toKey) {
+    return subMap(fromKey, true, toKey, false);
+  }
+
+  @Override
+  public NavigableMap<K, V2> tailMap(@ParametricNullness K fromKey) {
+    return tailMap(fromKey, true);
+  }
+
+  @Override
+  public NavigableMap<K, V2> tailMap(@ParametricNullness K fromKey, boolean inclusive) {
+    return Maps.transformEntries(fromMap().tailMap(fromKey, inclusive), transformer);
+  }
+
+  @Override
+  NavigableMap<K, V1> fromMap() {
+    return (NavigableMap<K, V1>) super.fromMap();
+  }
+
+  private @Nullable Entry<K, V2> transformEntry(@Nullable Entry<K, V1> entry) {
+    return (entry == null) ? null : Maps.transformEntry(transformer, entry);
+  }
+}

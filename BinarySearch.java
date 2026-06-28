1f8eec425387c733847e4ba1e4950f80b86acdd9
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 16:35:04 -0700

    codex-exp: iteration 10

--- BinarySearch.java
+++ BinarySearch.java
@@ -7,7 +7,6 @@ public final class BinarySearch {
     public static final int NOT_FOUND = -1;
     private static final String NOT_FOUND_MESSAGE = "Element is not present in array";
     private static final String FOUND_MESSAGE_PREFIX = "Element is present at index ";
-    private static final int[] DEMO_NUMBERS = { 2, 3, 4, 10, 40 };
     private static final int DEMO_TARGET = 10;
 
     private BinarySearch() {
@@ -42,9 +41,13 @@ public final class BinarySearch {
     }
 
     public static void main(String[] args) {
-        final int result = binarySearch(DEMO_NUMBERS, DEMO_TARGET);
+        final int result = binarySearch(demoNumbers(), DEMO_TARGET);
 
         System.out.println(formatSearchResult(result));
+    }
+
+    private static int[] demoNumbers() {
+        return new int[] { 2, 3, 4, 10, 40 };
     }
 
     private static String formatSearchResult(int result) {

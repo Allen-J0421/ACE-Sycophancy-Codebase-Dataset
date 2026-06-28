324aa5a0cb1c6d421711c630ab41a31c769e5b6c
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 16:40:35 -0700

    codex-exp: iteration 10

--- binary_search.java
+++ binary_search.java
@@ -38,30 +38,3 @@ final class BinarySearch {
         BinarySearchDemo.run();
     }
 }
-
-final class BinarySearchDemo {
-    private static final int SAMPLE_TARGET = 10;
-    private static final String NOT_FOUND_MESSAGE = "Element is not present in array";
-    private static final String FOUND_MESSAGE_PREFIX = "Element is present at index ";
-
-    private BinarySearchDemo() {
-    }
-
-    static void run() {
-        int index = BinarySearch.binarySearch(sampleValues(), SAMPLE_TARGET);
-
-        System.out.println(formatSearchResult(index));
-    }
-
-    private static int[] sampleValues() {
-        return new int[] { 2, 3, 4, 10, 40 };
-    }
-
-    private static String formatSearchResult(int index) {
-        if (!BinarySearch.isFound(index)) {
-            return NOT_FOUND_MESSAGE;
-        }
-
-        return FOUND_MESSAGE_PREFIX + index;
-    }
-}

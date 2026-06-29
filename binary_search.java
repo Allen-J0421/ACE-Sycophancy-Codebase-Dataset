83b31eeb2984ea80039c29c88830e4ecb2ac7d93
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 21:42:24 -0700

    codex-exp: iteration 2

--- binary_search.java
+++ binary_search.java
@@ -1,13 +1,16 @@
 class BinarySearch {
     private static final int NOT_FOUND = -1;
 
-    static int binarySearch(int[] values, int target) {
+    private BinarySearch() {
+    }
+
+    static int binarySearch(int[] sortedValues, int target) {
         int left = 0;
-        int right = values.length - 1;
+        int right = sortedValues.length - 1;
 
         while (left <= right) {
-            int middle = left + (right - left) / 2;
-            int candidate = values[middle];
+            int middle = middleIndex(left, right);
+            int candidate = sortedValues[middle];
 
             if (candidate == target) {
                 return middle;
@@ -23,19 +26,37 @@ class BinarySearch {
         return NOT_FOUND;
     }
 
+    static boolean isFound(int resultIndex) {
+        return resultIndex != NOT_FOUND;
+    }
+
+    private static int middleIndex(int left, int right) {
+        return left + (right - left) / 2;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchDemo.run();
+    }
+}
+
+class BinarySearchDemo {
+    private static final int[] SORTED_VALUES = { 2, 3, 4, 10, 40 };
+    private static final int TARGET = 10;
+
+    private BinarySearchDemo() {
+    }
+
+    static void run() {
+        int result = BinarySearch.binarySearch(SORTED_VALUES, TARGET);
+
+        System.out.println(formatSearchResult(result));
+    }
+
     private static String formatSearchResult(int resultIndex) {
-        if (resultIndex == NOT_FOUND) {
+        if (!BinarySearch.isFound(resultIndex)) {
             return "Element is not present in array";
         }
 
         return "Element is present at index " + resultIndex;
-    }
-
-    public static void main(String[] args) {
-        int[] sortedValues = { 2, 3, 4, 10, 40 };
-        int target = 10;
-        int result = binarySearch(sortedValues, target);
-
-        System.out.println(formatSearchResult(result));
     }
 }

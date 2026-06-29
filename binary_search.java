6cc612bddbf1a6e810beb0fee4bfba70355d4aa4
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-28 19:13:08 -0700

    codex-exp: iteration 1

--- binary_search.java
+++ binary_search.java
@@ -1,29 +1,44 @@
-class BinarySearch {
-    static int binarySearch(int arr[], int x) {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+final class BinarySearch {
+    private static final int NOT_FOUND = -1;
 
-            if (arr[mid] == x)
-                return mid;
-
-            if (arr[mid] < x)
-                low = mid + 1;
-
-            else
-                high = mid - 1;
-        }
-
-        return -1;
+    private BinarySearch() {
     }
 
-    public static void main(String args[]) {
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int x = 10;
-        int result = binarySearch(arr, x);
-        if (result == -1)
+    static int binarySearch(final int[] sortedValues, final int target) {
+        int low = 0;
+        int high = sortedValues.length - 1;
+
+        while (low <= high) {
+            final int mid = low + (high - low) / 2;
+            final int midValue = sortedValues[mid];
+
+            if (midValue == target) {
+                return mid;
+            }
+
+            if (midValue < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return NOT_FOUND;
+    }
+
+    public static void main(final String[] args) {
+        final int[] sortedValues = { 2, 3, 4, 10, 40 };
+        final int target = 10;
+        final int result = binarySearch(sortedValues, target);
+
+        printSearchResult(result);
+    }
+
+    private static void printSearchResult(final int index) {
+        if (index == NOT_FOUND) {
             System.out.println("Element is not present in array");
-        else
-            System.out.println("Element is present at index " + result);
+        } else {
+            System.out.println("Element is present at index " + index);
+        }
     }
 }

987c76d1e3504d96e05ee1109ee3d12470c575b2
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-29 21:25:57 -0700

    codex-exp: iteration 1

---
+++ BinarySearchService.java
@@ -0,0 +1,26 @@
+class BinarySearchService {
+    int search(int[] sortedValues, int target) {
+        if (sortedValues == null) {
+            throw new IllegalArgumentException("sortedValues must not be null");
+        }
+
+        int low = 0;
+        int high = sortedValues.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (sortedValues[mid] == target) {
+                return mid;
+            }
+
+            if (sortedValues[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- binary_search.java
+++ binary_search.java
@@ -1,29 +1,14 @@
 class BinarySearch {
-    static int binarySearch(int arr[], int x) {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+    public static void main(String[] args) {
+        BinarySearchService searchService = new BinarySearchService();
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int target = 10;
 
-            if (arr[mid] == x)
-                return mid;
-
-            if (arr[mid] < x)
-                low = mid + 1;
-
-            else
-                high = mid - 1;
-        }
-
-        return -1;
-    }
-
-    public static void main(String args[]) {
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int x = 10;
-        int result = binarySearch(arr, x);
-        if (result == -1)
+        int result = searchService.search(arr, target);
+        if (result == -1) {
             System.out.println("Element is not present in array");
-        else
+        } else {
             System.out.println("Element is present at index " + result);
+        }
     }
 }

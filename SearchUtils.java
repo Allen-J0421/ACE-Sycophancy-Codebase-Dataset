8c78826dc0bd5aba4211b829c1e40bc7631573fe
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-06-29 22:02:15 -0700

    codex-exp: iteration 4

--- SearchUtils.java
+++ SearchUtils.java
@@ -8,7 +8,7 @@ public final class SearchUtils {
 
     public static <T extends Comparable<? super T>> int binarySearch(
             T[] arr, T target, int fromIndex, int toIndex) {
-        checkRange(arr.length, fromIndex, toIndex);
+        RangeValidator.validate(arr.length, fromIndex, toIndex);
 
         int low = fromIndex;
         int high = toIndex - 1;
@@ -29,15 +29,5 @@ public final class SearchUtils {
         }
 
         return -1;
-    }
-
-    private static void checkRange(int length, int fromIndex, int toIndex) {
-        if (fromIndex > toIndex) {
-            throw new IllegalArgumentException("fromIndex must be less than or equal to toIndex");
-        }
-
-        if (fromIndex < 0 || toIndex > length) {
-            throw new IndexOutOfBoundsException("range must be within array bounds");
-        }
     }
 }

3a1af756a837279581c14f86304ba8425d7de239
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 16:24:21 -0700

    claude-exp: iteration 10

---
+++ TopologicalSortStrategy.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+interface TopologicalSortStrategy {
+    List<Integer> sort(GraphView graph);
+}
--- topological_sorting_bfs.java
+++ topological_sorting_bfs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.LinkedList;
 
 class CycleDetectedException extends RuntimeException {
     CycleDetectedException() {
@@ -10,54 +7,22 @@ class CycleDetectedException extends RuntimeException {
 }
 
 class TopologicalSortService {
+    private final TopologicalSortStrategy strategy;
 
-    private TopologicalSortService() {}
+    private TopologicalSortService(TopologicalSortStrategy strategy) {
+        this.strategy = strategy;
+    }
 
     static TopologicalSortService of() {
-        return new TopologicalSortService();
+        return new TopologicalSortService(new KahnTopologicalSortStrategy());
     }
 
-    private int[] computeIndegrees(GraphView graph) {
-        int n = graph.size();
-        int[] indegree = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (int next : graph.getNeighbors(i)) {
-                indegree[next]++;
-            }
-        }
-        return indegree;
-    }
-
-    private List<Integer> processQueue(GraphView graph, int[] indegree, Queue<Integer> q) {
-        List<Integer> result = new ArrayList<>();
-        while (!q.isEmpty()) {
-            int top = q.poll();
-            result.add(top);
-            for (int next : graph.getNeighbors(top)) {
-                if (--indegree[next] == 0) {
-                    q.add(next);
-                }
-            }
-        }
-        return result;
+    static TopologicalSortService of(TopologicalSortStrategy strategy) {
+        return new TopologicalSortService(strategy);
     }
 
     List<Integer> sort(GraphView graph) {
-        int n = graph.size();
-        int[] indegree = computeIndegrees(graph);
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-
-        List<Integer> result = processQueue(graph, indegree, q);
-
-        if (result.size() != n) {
-            throw new CycleDetectedException();
-        }
-        return result;
+        return strategy.sort(graph);
     }
 }
 

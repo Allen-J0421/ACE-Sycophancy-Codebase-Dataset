bb14e0b1e43c5c99afb69a04fedc00b197903b2b
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 16:52:33 -0700

    claude-exp: iteration 8

--- quick_sort.java
+++ quick_sort.java
@@ -1,29 +1,79 @@
-interface Sorter {
-    <T extends Comparable<T>> void sort(T[] arr);
-}
+class Sorting {
 
-interface PivotSelector {
-    <T extends Comparable<T>> int selectPivot(T[] arr, int low, int high);
+    interface Sorter {
+        <T extends Comparable<T>> void sort(T[] arr);
+    }
 
-    static PivotSelector last()   { return new Last(); }
-    static PivotSelector first()  { return new First(); }
-    static PivotSelector random() { return new Random(); }
+    interface PivotSelector {
+        <T extends Comparable<T>> int selectPivot(T[] arr, int low, int high);
+    }
 
-    class Last implements PivotSelector {
+    static Sorter quickSorter(PivotSelector selector) { return new QuickSorterImpl(selector); }
+
+    static PivotSelector lastPivot()   { return new LastPivotSelector(); }
+    static PivotSelector firstPivot()  { return new FirstPivotSelector(); }
+    static PivotSelector randomPivot() { return new RandomPivotSelector(); }
+
+    private static class QuickSorterImpl implements Sorter {
+
+        private final PivotSelector pivotSelector;
+
+        QuickSorterImpl(PivotSelector pivotSelector) {
+            this.pivotSelector = pivotSelector;
+        }
+
+        @Override
+        public <T extends Comparable<T>> void sort(T[] arr) {
+            if (arr != null && arr.length > 1) {
+                quickSort(arr, 0, arr.length - 1);
+            }
+        }
+
+        private <T extends Comparable<T>> void quickSort(T[] arr, int low, int high) {
+            if (low < high) {
+                int pi = partition(arr, low, high);
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
+
+        private <T extends Comparable<T>> int partition(T[] arr, int low, int high) {
+            int pivotIndex = pivotSelector.selectPivot(arr, low, high);
+            swap(arr, pivotIndex, high);
+            T pivot = arr[high];
+            int i = low - 1;
+            for (int j = low; j <= high - 1; j++) {
+                if (arr[j].compareTo(pivot) < 0) {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            return i + 1;
+        }
+
+        private static <T> void swap(T[] arr, int i, int j) {
+            T temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    private static class LastPivotSelector implements PivotSelector {
         @Override
         public <T extends Comparable<T>> int selectPivot(T[] arr, int low, int high) {
             return high;
         }
     }
 
-    class First implements PivotSelector {
+    private static class FirstPivotSelector implements PivotSelector {
         @Override
         public <T extends Comparable<T>> int selectPivot(T[] arr, int low, int high) {
             return low;
         }
     }
 
-    class Random implements PivotSelector {
+    private static class RandomPivotSelector implements PivotSelector {
         @Override
         public <T extends Comparable<T>> int selectPivot(T[] arr, int low, int high) {
             return low + (int) (Math.random() * (high - low + 1));
@@ -31,69 +81,21 @@ interface PivotSelector {
     }
 }
 
-class ArrayUtils {
-    static <T> void swap(T[] arr, int i, int j) {
-        T temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-}
-
-class QuickSorter implements Sorter {
-
-    private final PivotSelector pivotSelector;
-
-    QuickSorter(PivotSelector pivotSelector) {
-        this.pivotSelector = pivotSelector;
-    }
-
-    @Override
-    public <T extends Comparable<T>> void sort(T[] arr) {
-        if (arr != null && arr.length > 1) {
-            quickSort(arr, 0, arr.length - 1);
-        }
-    }
-
-    private <T extends Comparable<T>> void quickSort(T[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    private <T extends Comparable<T>> int partition(T[] arr, int low, int high) {
-        int pivotIndex = pivotSelector.selectPivot(arr, low, high);
-        ArrayUtils.swap(arr, pivotIndex, high);
-        T pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j].compareTo(pivot) < 0) {
-                i++;
-                ArrayUtils.swap(arr, i, j);
-            }
-        }
-        ArrayUtils.swap(arr, i + 1, high);
-        return i + 1;
-    }
-
-}
-
 class QuickSort {
 
     public static void main(String[] args) {
         Integer[] ints = {10, 7, 8, 9, 1, 5};
-        new QuickSorter(PivotSelector.last()).sort(ints);
+        Sorting.quickSorter(Sorting.lastPivot()).sort(ints);
         for (int val : ints) System.out.print(val + " ");
         System.out.println();
 
         String[] words = {"banana", "apple", "cherry", "date"};
-        new QuickSorter(PivotSelector.first()).sort(words);
+        Sorting.quickSorter(Sorting.firstPivot()).sort(words);
         for (String w : words) System.out.print(w + " ");
         System.out.println();
 
         Integer[] more = {3, 6, 8, 10, 1, 2, 1};
-        new QuickSorter(PivotSelector.random()).sort(more);
+        Sorting.quickSorter(Sorting.randomPivot()).sort(more);
         for (int val : more) System.out.print(val + " ");
         System.out.println();
     }

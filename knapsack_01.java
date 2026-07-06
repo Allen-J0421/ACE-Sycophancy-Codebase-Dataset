64bcc4a9046b6f7802f990835639e193f965e164
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 18:00:37 -0700

    claude-exp: iteration 1

--- knapsack_01.java
+++ knapsack_01.java
@@ -1,16 +1,28 @@
-class Knapsack {
+interface KnapsackStrategy {
+    int solve(int W, int[] val, int[] wt);
+}
 
-    static int knapsack(int W, int[] val, int[] wt) {
-
+class DPKnapsackStrategy implements KnapsackStrategy {
+    public int solve(int W, int[] val, int[] wt) {
         int[] dp = new int[W + 1];
-
         for (int i = 1; i <= wt.length; i++) {
-
             for (int j = W; j >= wt[i - 1]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - wt[i - 1]] + val[i - 1]);
             }
         }
         return dp[W];
+    }
+}
+
+class Knapsack {
+    private final KnapsackStrategy strategy;
+
+    Knapsack(KnapsackStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    int solve(int W, int[] val, int[] wt) {
+        return strategy.solve(W, val, wt);
     }
 
     public static void main(String[] args) {
@@ -18,6 +30,7 @@ class Knapsack {
         int[] wt = {4, 5, 1};
         int W = 4;
 
-        System.out.println(knapsack(W, val, wt));
+        Knapsack knapsack = new Knapsack(new DPKnapsackStrategy());
+        System.out.println(knapsack.solve(W, val, wt));
     }
 }

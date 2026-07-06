0f680eb13200670ee8a6f30e124e8192f2245176
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 17:01:26 -0700

    claude-exp: iteration 7

--- MinHeap.java
+++ MinHeap.java
@@ -80,4 +80,49 @@ class MinHeap<T extends Comparable<T>> implements Iterable<T> {
     public Iterator<T> iterator() {
         return heapArray.iterator();
     }
+
+    private static class HeapHelper<T extends Comparable<T>> {
+
+        private final ArrayList<T> heap;
+
+        HeapHelper(ArrayList<T> heap) {
+            this.heap = heap;
+        }
+
+        void siftUp(int index) {
+            while (index != 0 && heap.get(index).compareTo(heap.get(parent(index))) < 0) {
+                swap(index, parent(index));
+                index = parent(index);
+            }
+        }
+
+        void siftDown(int index) {
+            int size = heap.size();
+            int smallest = index;
+            int l = 2 * index + 1;
+            int r = 2 * index + 2;
+
+            if (l < size && heap.get(l).compareTo(heap.get(smallest)) < 0) {
+                smallest = l;
+            }
+            if (r < size && heap.get(r).compareTo(heap.get(smallest)) < 0) {
+                smallest = r;
+            }
+
+            if (smallest != index) {
+                swap(index, smallest);
+                siftDown(smallest);
+            }
+        }
+
+        void swap(int a, int b) {
+            T temp = heap.get(a);
+            heap.set(a, heap.get(b));
+            heap.set(b, temp);
+        }
+
+        int parent(int index) {
+            return (index - 1) / 2;
+        }
+    }
 }

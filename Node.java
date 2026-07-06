cdd5e54f703d406100ab1c13b4f7f3dddd2dbf4a
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 17:14:44 -0700

    claude-exp: iteration 3

--- Node.java
+++ Node.java
@@ -1,15 +1,9 @@
 class Node<T> {
-    private T data;
-    private Node<T> left;
-    private Node<T> right;
+    T data;
+    Node<T> left;
+    Node<T> right;
 
     Node(T data) {
         this.data = data;
     }
-
-    T getData() { return data; }
-    Node<T> getLeft() { return left; }
-    Node<T> getRight() { return right; }
-    void setLeft(Node<T> left) { this.left = left; }
-    void setRight(Node<T> right) { this.right = right; }
 }
--- binary_search_tree_searching.java
+++ binary_search_tree_searching.java
@@ -7,22 +7,36 @@ class BinarySearchTree<T extends Comparable<T>> implements SearchTree<T> {
 
     private Node<T> insertRec(Node<T> node, T key) {
         if (node == null) return new Node<>(key);
-        int cmp = key.compareTo(node.getData());
+        int cmp = key.compareTo(node.data);
         if (cmp < 0)
-            node.setLeft(insertRec(node.getLeft(), key));
+            node.left = insertRec(node.left, key);
         else if (cmp > 0)
-            node.setRight(insertRec(node.getRight(), key));
+            node.right = insertRec(node.right, key);
         return node;
     }
 
     public boolean search(T key) {
         Node<T> current = root;
         while (current != null) {
-            int cmp = key.compareTo(current.getData());
+            int cmp = key.compareTo(current.data);
             if (cmp == 0) return true;
-            current = cmp > 0 ? current.getRight() : current.getLeft();
+            current = cmp > 0 ? current.right : current.left;
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb, 0);
+        return sb.toString().stripTrailing();
+    }
+
+    private void buildString(Node<T> node, StringBuilder sb, int depth) {
+        if (node == null) return;
+        buildString(node.right, sb, depth + 1);
+        sb.append("    ".repeat(depth)).append(node.data).append("\n");
+        buildString(node.left, sb, depth + 1);
     }
 
     public static void main(String[] args) {
@@ -33,7 +47,7 @@ class BinarySearchTree<T extends Comparable<T>> implements SearchTree<T> {
         bst.insert(7);
         bst.insert(9);
 
-        int key = 7;
-        System.out.println(bst.search(key));
+        System.out.println(bst.search(7));
+        System.out.println(bst);
     }
 }

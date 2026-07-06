cac2a17c3fc9beddc52b8537541f731a10d0da8e
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 18:13:08 -0700

    claude-exp: iteration 1

--- matrix_chain_multiplication.java
+++ matrix_chain_multiplication.java
@@ -1,34 +1,55 @@
-class MatrixChainMultiplication {
+class MatrixChainResult {
+    final int minCost;
+    final int[][] splitTable;
 
-    static int matrixMultiplication(int[] arr)
-    {
+    MatrixChainResult(int minCost, int[][] splitTable) {
+        this.minCost = minCost;
+        this.splitTable = splitTable;
+    }
+}
 
-        int n = arr.length;
+class MatrixChainSolver {
 
-        int[][] dp = new int[n][n];
+    MatrixChainResult solve(int[] dims) {
+        int n = dims.length;
+        int[][] cost = new int[n][n];
+        int[][] split = new int[n][n];
 
         for (int len = 2; len < n; len++) {
             for (int i = 0; i < n - len; i++) {
                 int j = i + len;
-                dp[i][j] = Integer.MAX_VALUE;
+                cost[i][j] = Integer.MAX_VALUE;
 
                 for (int k = i + 1; k < j; k++) {
-                    int cost = dp[i][k] + dp[k][j]
-                               + arr[i] * arr[k] * arr[j];
-                    if (cost < dp[i][j]) {
-                        dp[i][j] = cost;
+                    int c = cost[i][k] + cost[k][j] + dims[i] * dims[k] * dims[j];
+                    if (c < cost[i][j]) {
+                        cost[i][j] = c;
+                        split[i][j] = k;
                     }
                 }
             }
         }
 
-        return dp[0][n - 1];
+        return new MatrixChainResult(cost[0][n - 1], split);
     }
 
-    public static void main(String[] args)
-    {
+    String optimalOrder(int[][] split, int i, int j) {
+        if (i + 1 == j) {
+            return "M" + j;
+        }
+        int k = split[i][j];
+        return "(" + optimalOrder(split, i, k) + " x " + optimalOrder(split, k, j) + ")";
+    }
+}
 
-        int[] arr = { 2, 1, 3, 4 };
-        System.out.println(matrixMultiplication(arr));
+class MatrixChainMultiplication {
+
+    public static void main(String[] args) {
+        int[] dims = { 2, 1, 3, 4 };
+
+        MatrixChainSolver solver = new MatrixChainSolver();
+        MatrixChainResult result = solver.solve(dims);
+
+        System.out.println(result.minCost);
     }
 }

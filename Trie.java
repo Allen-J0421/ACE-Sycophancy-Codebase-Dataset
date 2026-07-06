c29c47ef4901f505c52040c702977b87fc92fb27
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 17:28:23 -0700

    claude-exp: iteration 4

--- Trie.java
+++ Trie.java
@@ -46,22 +46,4 @@ public class Trie {
     {
         return navigate(prefix) != null;
     }
-
-    public static void main(String[] args)
-    {
-        Trie trie = new Trie();
-        String[] arr = {"and", "ant", "do", "dad"};
-        for (String s : arr) {
-            trie.insert(s);
-        }
-        String[] searchKeys = {"do", "gee", "bat"};
-        for (String s : searchKeys) {
-            System.out.print(trie.search(s) + " ");
-        }
-        System.out.println();
-        String[] prefixKeys = {"ge", "ba", "do", "de"};
-        for (String s : prefixKeys) {
-            System.out.print(trie.isPrefix(s) + " ");
-        }
-    }
 }

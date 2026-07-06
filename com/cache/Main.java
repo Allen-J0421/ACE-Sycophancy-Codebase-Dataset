cf9dff1b169dd961183b98c8a9107bf43e08172c
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-05 17:58:46 -0700

    claude-exp: iteration 9

--- com/cache/Main.java
+++ com/cache/Main.java
@@ -1,6 +1,8 @@
+package com.cache;
+
 public class Main {
     public static void main(String[] args) {
-        LRUCache<Integer, Integer> cache = new LRUCache<>(2);
+        Cache<Integer, Integer> cache = new LRUCache<>(2);
 
         cache.put(1, 1);
         cache.put(2, 2);

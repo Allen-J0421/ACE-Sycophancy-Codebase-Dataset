f0f56a51117a041ce0b783c7515c7ffad6ddb19c
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-06 08:22:41 -0700

    claude-exp: iteration 5

--- red_black_tree_insertion.java
+++ red_black_tree_insertion.java
@@ -147,58 +147,76 @@ public class RedBlackTree<T extends Comparable<T>> {
         }
     }
 
+    static class InsertionController<T extends Comparable<T>>
+    {
+        private final RedBlackTree<T> tree;
+
+        InsertionController(RedBlackTree<T> tree)
+        {
+            this.tree = tree;
+        }
+
+        public void insert(T data)
+        {
+            if(tree.root == null)
+            {
+                tree.root = new Node<>(data);
+                tree.root.setBlack();
+            }
+            else
+            {
+                RotationHandler<T> handler = new RotationHandler<>();
+                tree.root = insertHelp(tree.root, data, handler);
+            }
+        }
+
+        private Node<T> insertHelp(Node<T> root, T data, RotationHandler<T> handler)
+        {
+            boolean f = false;
+
+            if(root == null)
+                return new Node<>(data);
+            else if(data.compareTo(root.data) < 0)
+            {
+                root.left = insertHelp(root.left, data, handler);
+                root.left.parent = root;
+                if(root != tree.root)
+                {
+                    if(root.isRed() && root.left.isRed())
+                        f = true;
+                }
+            }
+            else
+            {
+                root.right = insertHelp(root.right, data, handler);
+                root.right.parent = root;
+                if(root != tree.root)
+                {
+                    if(root.isRed() && root.right.isRed())
+                        f = true;
+                }
+            }
+
+            root = handler.applyPending(root);
+
+            if(f)
+                handler.handleViolation(root, tree.root);
+
+            return root;
+        }
+    }
+
+    private final InsertionController<T> controller;
+
     public RedBlackTree()
     {
         root = null;
-    }
-
-    Node<T> insertHelp(Node<T> root, T data, RotationHandler<T> handler)
-    {
-        boolean f = false;
-
-        if(root == null)
-            return new Node<>(data);
-        else if(data.compareTo(root.data) < 0)
-        {
-            root.left = insertHelp(root.left, data, handler);
-            root.left.parent = root;
-            if(root != this.root)
-            {
-                if(root.isRed() && root.left.isRed())
-                    f = true;
-            }
-        }
-        else
-        {
-            root.right = insertHelp(root.right, data, handler);
-            root.right.parent = root;
-            if(root != this.root)
-            {
-                if(root.isRed() && root.right.isRed())
-                    f = true;
-            }
-        }
-
-        root = handler.applyPending(root);
-
-        if(f)
-            handler.handleViolation(root, this.root);
-
-        return root;
+        controller = new InsertionController<>(this);
     }
 
     public void insert(T data)
     {
-        if(this.root == null)
-        {
-            this.root = new Node<>(data);
-            this.root.setBlack();
-        }
-        else
-        {
-            RotationHandler<T> handler = new RotationHandler<>();
-            this.root = insertHelp(this.root, data, handler);
-        }
+        controller.insert(data);
     }
 
     public static void main(String[] args)

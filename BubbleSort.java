a570cbff7fc21338636e21c60821591752e957b5
Allen Jiang <allenjiang@Atuls-iMac.local>
2026-07-06 23:21:25 -0700

    claude-exp: iteration 3

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+class BubbleSort<T extends Comparable<T>> implements Sorter<T> {
+
+    @Override
+    public void sort(T[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    swap(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    private void swap(T[] arr, int i, int j) {
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
--- bubble_sort.java
+++ bubble_sort.java
@@ -1,32 +1,18 @@
 import java.util.Arrays;
 
-class BubbleSort<T extends Comparable<T>> implements Sorter<T> {
-
-    @Override
-    public void sort(T[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j].compareTo(arr[j + 1]) > 0) {
-                    swap(arr, j, j + 1);
-                    swapped = true;
-                }
-            }
-            if (!swapped) break;
-        }
-    }
-
-    private void swap(T[] arr, int i, int j) {
-        T temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+class Main {
 
     public static void main(String[] args) {
-        Sorter<Integer> sorter = new BubbleSort<>();
-        Integer[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        runCase("Random order", new Integer[]{ 64, 34, 25, 12, 22, 11, 90 });
+        runCase("Already sorted", new Integer[]{ 1, 2, 3, 4, 5 });
+        runCase("Reverse order", new Integer[]{ 5, 4, 3, 2, 1 });
+        runCase("Single element", new Integer[]{ 42 });
+        runCase("Strings", new String[]{ "banana", "apple", "cherry" });
+    }
+
+    private static <T extends Comparable<T>> void runCase(String label, T[] arr) {
+        Sorter<T> sorter = new BubbleSort<>();
         sorter.sort(arr);
-        System.out.println("Sorted array: " + Arrays.toString(arr));
+        System.out.println(label + ": " + Arrays.toString(arr));
     }
 }
